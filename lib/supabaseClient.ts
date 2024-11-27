a5fa0612bb8021e2fc6f24224681b6698e78e935
Yukash <sivarajyukash@gmail.com>
2024-11-27 15:14:06 +1100

    lol keys were not in env

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://pjxnzjnbnpstpeoimztk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqeG56am5ibnBzdHBlb2ltenRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2Nzk2MjAsImV4cCI6MjA0ODI1NTYyMH0.zIkOSFr0sohOa-2-V8zHopLVp1bEaVs-Bw1j25Uh494';
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
